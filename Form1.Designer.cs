3ed13cda21483e2b9a75ef08874954fbc3b49dd4
Leonardo Lenis <leonardo.lenis@masglobalconsulting.com>
2023-01-02 13:19:26 -0800

    Updated btn

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.txtId = new System.Windows.Forms.TextBox();
             this.btn_buscar = new System.Windows.Forms.Button();
@@ -46,12 +47,13 @@
             this.btnActualizar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnLimpiar = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.button7);
             this.groupBox3.Controls.Add(this.button6);
             this.groupBox3.Controls.Add(this.button1);
             this.groupBox3.Controls.Add(this.txtId);
@@ -73,6 +75,15 @@
             this.groupBox3.TabIndex = 0;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Datos de producto";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(56, 287);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(166, 51);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
@@ -147,7 +158,7 @@
             this.label10.AutoSize = true;
             this.label10.Location = new System.Drawing.Point(41, 462);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(182, 32);
+            this.label10.Size = new System.Drawing.Size(118, 21);
             this.label10.TabIndex = 4;
             this.label10.Text = "Existencias:";
             // 
@@ -156,7 +167,7 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(41, 389);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(224, 32);
+            this.label9.Size = new System.Drawing.Size(145, 21);
             this.label9.TabIndex = 3;
             this.label9.Text = "Precio Público:";
             // 
@@ -166,7 +177,7 @@
             this.label8.Font = new System.Drawing.Font("Cascadia Code SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(41, 220);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(182, 32);
+            this.label8.Size = new System.Drawing.Size(118, 21);
             this.label8.TabIndex = 2;
             this.label8.Text = "Descripción:";
             // 
@@ -176,7 +187,7 @@
             this.label7.Font = new System.Drawing.Font("Cascadia Code SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(41, 152);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(112, 32);
+            this.label7.Size = new System.Drawing.Size(73, 21);
             this.label7.TabIndex = 1;
             this.label7.Text = "Nombre:";
             // 
@@ -186,7 +197,7 @@
             this.label6.Font = new System.Drawing.Font("Cascadia Code SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(41, 80);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(112, 32);
+            this.label6.Size = new System.Drawing.Size(73, 21);
             this.label6.TabIndex = 0;
             this.label6.Text = "Código:";
             // 
@@ -230,14 +241,14 @@
             this.btnLimpiar.UseVisualStyleBackColor = true;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
-            // button6
+            // button7
             // 
-            this.button6.Location = new System.Drawing.Point(56, 287);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(166, 51);
-            this.button6.TabIndex = 13;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button7.Location = new System.Drawing.Point(133, 136);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(88, 47);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -293,6 +304,7 @@
         private System.Windows.Forms.TextBox txtId;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
